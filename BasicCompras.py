b4e3965b7cc9aae1b51ac8dab58888ebe8df74d2
Xannax-Dev <danielstevenobredorivera@gmail.com>
2025-02-20 10:30:25 -0500

    MadLibgames and a BasicCompras System im starting learning python :)

---
+++ BasicCompras.py
@@ -0,0 +1,8 @@
+item = input("Que producto le gustaria comprar: ")
+price = float(input("Cual es el precio de su procuto: "))
+quantity = float(input("Cuantos le gustaria? : "))
+
+total = price * quantity
+
+print(f"compraste {quantity} {item}/s")
+print(f"total {total}")
---
+++ madlibgame.py
@@ -0,0 +1,17 @@
+#MadLibs game
+#Word game where you create a story
+# by filling in blank with random words
+
+
+adjective1 = input("Introduce un adjetivo (Descripcion): ")
+adjective2 = input("Introduce un adjetivo (Descripcion): ")
+noun1 = input("Introduce un Sustantivo (persona, lugar o cosa): ")
+verb1 = input("Introduce un verbo (accion)")
+adjective3 = input("Introduce un adjetivo (Descripcion): ")
+
+
+
+print(f"Hoy fui al {adjective1} zoologico.");
+print(f"En la exhibicion, vi un {noun1}")
+print(f"{noun1} fue  {adjective2} y {verb1} ")
+print(f"yo estaba {adjective3}!")
